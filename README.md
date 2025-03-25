48ac2ddef08f15c46dd8e18ac2a93a311ba34c8a
tdue21 <tho.due@gmail.com>
2025-03-25 22:56:10 +0100

    Added another test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Dove.Blog
 
 [![Build](https://github.com/Tdue21/Dove.Blog/actions/workflows/build.yml/badge.svg)](https://github.com/Tdue21/Dove.Blog/actions/workflows/build.yml)
+
+![Tests passed successfully](https://img.shields.io/badge/tests-1%20passed-success)
--- Source/Dove.Blog.Tests/PageDataProviderTests.cs
+++ Source/Dove.Blog.Tests/PageDataProviderTests.cs
@@ -2,6 +2,7 @@ using Dove.Blog.Abstractions;
 using Dove.Blog.Logic;
 using FluentAssertions;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 
 namespace Dove.Blog.Tests;
 
@@ -18,5 +19,16 @@ public class PageDataProviderTests
         page.Should().NotBeNull();
         page.Content.Should().NotBeNullOrEmpty();
         page.Content.Should().BeEquivalentTo("<h2>Hello, World!</h2>\n");
+    }
+
+    [Fact]
+    public async Task GetPage_Page_Not_Found_TestAsync()
+    {
+        var fileProvider = Substitute.For<IDataProvider>();
+        fileProvider.ReadPageContent(Arg.Any<string>()).Throws<FileNotFoundException>();
+
+        var provider = new PageDataProvider(fileProvider);
+
+        await provider.Invoking(x => x.GetPage("Index")).Should().ThrowAsync<FileNotFoundException>();
     }
 }
